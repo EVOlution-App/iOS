f802be731a295fff2854560e6f957499d2a17b56
Thiago Holanda <unnamedd@gmail.com>
2017-03-01 17:25:00 -0300

    Initial Commit

---
+++ swift-evolution/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-evolution
+//
+//  Created by Thiago Holanda on 01/03/17.
+//  Copyright © 2017 Holanda Mobile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
