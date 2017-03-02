5f19ac1a8e1d86c9fb935233478261a3bdf76e49
Thiago Holanda <unnamedd@gmail.com>
2017-03-02 01:02:46 -0300

    changed the type of state from String to StatusState

--- swift-evolution/sourcecode/models/Status.swift
+++ swift-evolution/sourcecode/models/Status.swift
@@ -1,14 +1,13 @@
-import UIKit
 import Unbox
 
 struct Status {
     let version: String?
-    let state: String?
+    let state: StatusState?
 }
 
 extension Status: Unboxable {
     init(unboxer: Unboxer) throws {
         self.version = unboxer.unbox(key: "version")
-        self.state = unboxer.unbox(key: "state")
+        self.state = unboxer.unbox(key: "state", formatter: StateFormatter())
     }
 }
