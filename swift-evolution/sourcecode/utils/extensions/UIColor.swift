b98c799f62026a31f2d4e37256b4404152e27205
Thiago Holanda <unnamedd@gmail.com>
2017-03-02 03:16:08 -0300

    added extension with status colors

--- swift-evolution/sourcecode/utils/extensions/UIColor.swift
+++ swift-evolution/sourcecode/utils/extensions/UIColor.swift
@@ -1,13 +1,55 @@
-//
-//  UIColor.swift
-//  swift-evolution
-//
-//  Created by Thiago Holanda on 02/03/17.
-//  Copyright © 2017 Holanda Mobile. All rights reserved.
-//
-
 import UIKit
 
-class UIColor: UIColor {
-
+public extension UIColor {
+    
+    // Color for status: **awaitingReview**
+    public static var awaitingReview: UIColor {
+        return UIColor(red: 255, green: 149, blue: 0, alpha: 1)
+    }
+    
+    // Color for status: **scheduledForReview**
+    public static var scheduledForReview: UIColor {
+        return UIColor(red: 255, green: 149, blue: 0, alpha: 1)
+    }
+    
+    // Color for status: **activeReview**
+    public static var activeReview: UIColor {
+        return UIColor(red: 255, green: 149, blue: 0, alpha: 1)
+    }
+    
+    // Color for status: **returnedForRevision**}
+    public static var returnedForRevision: UIColor {
+        return UIColor(red: 255, green: 149, blue: 0, alpha: 1)
+    }
+    // Color for status: **deferred**
+    public static var deferred: UIColor {
+        return UIColor(red: 88, green: 86, blue: 214, alpha: 1)
+    }
+    
+    // Color for status: **accepted**}
+    public static var accepted: UIColor {
+        return UIColor(red: 76, green: 217, blue: 100, alpha: 1)
+    }
+    
+    // Color for status: **acceptedWithRevisions**
+    public static var acceptedWithRevisions: UIColor {
+        return UIColor(red: 76, green: 217, blue: 100, alpha: 1)
+    }
+    
+    // Color for status: **rejected**
+    public static var rejected: UIColor {
+        return UIColor(red: 255, green: 59, blue: 48, alpha: 1)
+    }
+    
+    // Color for status: **implemented**}
+    public static var implemented: UIColor {
+        return UIColor(red: 0, green: 122, blue: 255, alpha: 1)
+    }
+    
+    // Color for status: **withdrawn**}
+    public static var withdrawn: UIColor {
+        return UIColor(red: 255, green: 59, blue: 48, alpha: 1)
+    }
 }
+ 
+ 
