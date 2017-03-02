d4065af9f00f0d23a9dc7ad3a1d72d418390afb9
Thiago Holanda <unnamedd@gmail.com>
2017-03-02 03:13:43 -0300

    added swift logo to assets

---
+++ swift-evolution/sourcecode/controllers/evolutions/list/ListEvolutionsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ListEvolutionsViewController.swift
+//  swift-evolution
+//
+//  Created by Thiago Holanda on 02/03/17.
+//  Copyright © 2017 Holanda Mobile. All rights reserved.
+//
+
+import UIKit
+
+class ListEvolutionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ swift-evolution/sourcecode/utils/extensions/UIColor.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor.swift
+//  swift-evolution
+//
+//  Created by Thiago Holanda on 02/03/17.
+//  Copyright © 2017 Holanda Mobile. All rights reserved.
+//
+
+import UIKit
+
+class UIColor: UIColor {
+
+}
