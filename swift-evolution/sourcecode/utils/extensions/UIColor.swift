18c6242dfbe24cc5c17bb9e07fcbc4fe85aa59c6
Thiago Holanda <unnamedd@gmail.com>
2017-03-03 00:22:34 -0300

    fix math from colors

--- swift-evolution/sourcecode/utils/extensions/UIColor.swift
+++ swift-evolution/sourcecode/utils/extensions/UIColor.swift
@@ -4,52 +4,52 @@ public extension UIColor {
     
     // Color for status: **awaitingReview**
     public static var awaitingReview: UIColor {
-        return UIColor(red: 255, green: 149, blue: 0, alpha: 1)
+        return UIColor(red: 255/255, green: 149/255, blue: 0/255, alpha: 1)
     }
     
     // Color for status: **scheduledForReview**
     public static var scheduledForReview: UIColor {
-        return UIColor(red: 255, green: 149, blue: 0, alpha: 1)
+        return UIColor(red: 255/255, green: 149/255, blue: 0/255, alpha: 1)
     }
     
     // Color for status: **activeReview**
     public static var activeReview: UIColor {
-        return UIColor(red: 255, green: 149, blue: 0, alpha: 1)
+        return UIColor(red: 255/255, green: 149/255, blue: 0/255, alpha: 1)
     }
     
     // Color for status: **returnedForRevision**}
     public static var returnedForRevision: UIColor {
-        return UIColor(red: 255, green: 149, blue: 0, alpha: 1)
+        return UIColor(red: 255/255, green: 149/255, blue: 0/255, alpha: 1)
     }
     // Color for status: **deferred**
     public static var deferred: UIColor {
-        return UIColor(red: 88, green: 86, blue: 214, alpha: 1)
+        return UIColor(red: 88/255, green: 86/255, blue: 214/255, alpha: 1)
     }
     
     // Color for status: **accepted**}
     public static var accepted: UIColor {
-        return UIColor(red: 76, green: 217, blue: 100, alpha: 1)
+        return UIColor(red: 76/255, green: 217/255, blue: 100/255, alpha: 1)
     }
     
     // Color for status: **acceptedWithRevisions**
     public static var acceptedWithRevisions: UIColor {
-        return UIColor(red: 76, green: 217, blue: 100, alpha: 1)
+        return UIColor(red: 76/255, green: 217/255, blue: 100/255, alpha: 1)
     }
     
     // Color for status: **rejected**
     public static var rejected: UIColor {
-        return UIColor(red: 255, green: 59, blue: 48, alpha: 1)
+        return UIColor(red: 255/255, green: 59/255, blue: 48/255, alpha: 1)
     }
     
     // Color for status: **implemented**}
     public static var implemented: UIColor {
-        return UIColor(red: 0, green: 122, blue: 255, alpha: 1)
+        return UIColor(red: 0/255, green: 122/255, blue: 255/255, alpha: 1)
     }
     
     // Color for status: **withdrawn**}
     public static var withdrawn: UIColor {
-        return UIColor(red: 255, green: 59, blue: 48, alpha: 1)
+        return UIColor(red: 255/255, green: 59/255, blue: 48/255, alpha: 1)
     }
 }
- 
- 
+
+
