be6d84b6168dce17e96b4e2f35ba19972947818c
Thiago Holanda <unnamedd@gmail.com>
2017-03-03 18:45:50 -0300

    added a public struct ‘Status’ into UIColor

--- swift-evolution/sourcecode/models/State.swift
+++ swift-evolution/sourcecode/models/State.swift
@@ -79,34 +79,34 @@ extension StatusState: RawRepresentable {
     public var rawValue: RawValue {
         switch self {
         case .awaitingReview:
-            return State(name: "Awaiting Review", shortName: "Awaiting Review", className: "awaiting-review", identifier: ".awaitingReview", color: UIColor.awaitingReview)
+            return State(name: "Awaiting Review", shortName: "Awaiting Review", className: "awaiting-review", identifier: ".awaitingReview", color: UIColor.Status.awaitingReview)
             
         case .scheduledForReview:
-            return State(name: "Scheduled for Review", shortName: "Scheduled", className: "scheduled-for-review", identifier: ".scheduledForReview", color: UIColor.scheduledForReview)
+            return State(name: "Scheduled for Review", shortName: "Scheduled", className: "scheduled-for-review", identifier: ".scheduledForReview", color: UIColor.Status.scheduledForReview)
             
         case .activeReview:
-            return State(name: "Active Review", shortName: "Active Review", className: "active-review", identifier: ".activeReview", color: UIColor.activeReview)
+            return State(name: "Active Review", shortName: "Active Review", className: "active-review", identifier: ".activeReview", color: UIColor.Status.activeReview)
             
         case .returnedForRevision:
-            return State(name: "Returned for Revision", shortName: "Returned", className: "returned-for-revision", identifier: ".returnedForRevision", color: UIColor.returnedForRevision)
+            return State(name: "Returned for Revision", shortName: "Returned", className: "returned-for-revision", identifier: ".returnedForRevision", color: UIColor.Status.returnedForRevision)
             
         case .withdrawn:
-            return State(name: "Withdrawn", shortName: "Withdrawn", className: "withdrawn", identifier: ".withdrawn", color: UIColor.withdrawn)
+            return State(name: "Withdrawn", shortName: "Withdrawn", className: "withdrawn", identifier: ".withdrawn", color: UIColor.Status.withdrawn)
             
         case .deferred:
-            return State(name: "Deferred", shortName: "Deferred", className: "deferred", identifier: ".deferred", color: UIColor.deferred)
+            return State(name: "Deferred", shortName: "Deferred", className: "deferred", identifier: ".deferred", color: UIColor.Status.deferred)
             
         case .accepted:
-            return State(name: "Accepted", shortName: "Accepted", className: "accepted", identifier: ".accepted", color: UIColor.accepted)
+            return State(name: "Accepted", shortName: "Accepted", className: "accepted", identifier: ".accepted", color: UIColor.Status.accepted)
             
         case .acceptedWithRevisions:
-            return State(name: "Accepted with revisions", shortName: "Accepted", className: "accepted-with-revisions", identifier: ".acceptedWithRevisions", color: UIColor.acceptedWithRevisions)
+            return State(name: "Accepted with revisions", shortName: "Accepted", className: "accepted-with-revisions", identifier: ".acceptedWithRevisions", color: UIColor.Status.acceptedWithRevisions)
             
         case .rejected:
-            return State(name: "Rejected", shortName: "Rejected", className: "rejected", identifier: ".rejected", color: UIColor.rejected)
+            return State(name: "Rejected", shortName: "Rejected", className: "rejected", identifier: ".rejected", color: UIColor.Status.rejected)
             
         case .implemented:
-            return State(name: "Implemented", shortName: "Implemented", className: "implemented", identifier: ".implemented", color: UIColor.implemented)
+            return State(name: "Implemented", shortName: "Implemented", className: "implemented", identifier: ".implemented", color: UIColor.Status.implemented)
             
         case .error:
             return State(name: "Error", shortName: "Error", className: "error", identifier: ".error", color: UIColor.clear)
