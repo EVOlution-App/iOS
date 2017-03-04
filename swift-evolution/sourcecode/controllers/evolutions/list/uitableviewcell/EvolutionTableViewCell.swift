2e8306ce7168db64e4a32c0ea58fd69a0b9265e1
Thiago Holanda <unnamedd@gmail.com>
2017-03-03 23:34:34 -0300

    #1 implemented the style to tags on evolutionTableViewCell

--- swift-evolution/sourcecode/controllers/evolutions/list/uitableviewcell/EvolutionTableViewCell.swift
+++ swift-evolution/sourcecode/controllers/evolutions/list/uitableviewcell/EvolutionTableViewCell.swift
@@ -1,16 +1,34 @@
 import UIKit
+import SwiftRichString
+
+enum Tag: String {
+    case content = "content"
+    case title = "title"
+    case value = "value"
+    case id = "id"
+    
+    func wrap(string: String) -> String {
+        return "<\(self.rawValue)>\(string)</\(self.rawValue)>"
+    }
+}
 
 extension String {
     static var newLine: String {
         return "\n"
     }
+    
+    static var doubleSpace: String {
+        return "  "
+    }
+    
+    func tag(_ tag: Tag) -> String {
+        return tag.wrap(string: self)
+    }
 }
 
 class EvolutionTableViewCell: UITableViewCell, CellProtocol {
-
+    
     @IBOutlet private weak var statusLabel: StatusLabel!
-    @IBOutlet private weak var idLabel: UILabel!
-    @IBOutlet private weak var nameLabel: UILabel!
     @IBOutlet private weak var detailsLabel: UILabel!
     
     static var cellIdentifier: String {
@@ -33,7 +51,7 @@ class EvolutionTableViewCell: UITableViewCell, CellProtocol {
         
     }
 
-    func configureElements() {
+    private func configureElements() {
         guard let proposal = self.proposal else {
             return
         }
@@ -42,38 +60,65 @@ class EvolutionTableViewCell: UITableViewCell, CellProtocol {
         self.statusLabel.borderColor = state.color
         self.statusLabel.textColor = state.color
         self.statusLabel.text = state.name
-            
-        self.idLabel.text = proposal.id
-        self.nameLabel.text = proposal.title
         
         var details = ""
         
+        details += proposal.id.tag(.id)
+        details += String.doubleSpace
+        details += proposal.title.trimmingCharacters(in: .whitespacesAndNewlines).tag(.title)
+        
         // Render Authors
         if let authors = self.renderAuthors() {
-            details += authors + String.newLine
+            details += String.newLine + String.newLine
+            details += authors
         }
         
         // Render Review Manager
         if let reviewer = proposal.reviewManager, let name = reviewer.name, name != "" {
-            details += "Review Manager: " + name + String.newLine
+            details += String.newLine + "Review Manager:" + String.doubleSpace + name.tag(.value)
         }
         
         // Render Bugs
         if let bugs = self.renderBugs() {
-            details += bugs + String.newLine
+            details += String.newLine + bugs
         }
         
         // Render Implemented Proposal
         if proposal.status.state == .implemented, let version = proposal.status.version {
-            details += "Implemented In: Swift \(version)"
+            details += String.newLine + "Implemented In:" + String.doubleSpace + "Swift \(version)".tag(.value)
         }
         
-        self.detailsLabel.text = details
+        let defaultStyle = Style("defaultStyle", {
+            $0.lineSpacing = 5.5
+            $0.hyphenationFactor = 1.0
+        })
+        
+        if let tagged = try? MarkupString(source: details) {
+            self.detailsLabel.attributedText = tagged.render(withStyles: self.styles()).add(style: defaultStyle)
+        }
     }
     
+    private func styles() -> [Style] {
+        let id = Style("id", {
+            $0.font = FontAttribute(.HelveticaNeue, size: 20)
+            $0.color = UIColor.Proposal.lightGray
+            $0.lineSpacing = 0
+        })
+        
+        let title = Style("title", {
+            $0.font = FontAttribute(.HelveticaNeue, size: 20)
+            $0.color = UIColor.Proposal.darkGray
+            $0.lineSpacing = 0
+        })
+
+        let value = Style("value", {
+            $0.color = UIColor.Proposal.darkGray
+        })
+
+        return [id, title, value]
+    }
     
-    
-    func renderAuthors() -> String? {
+    private func renderAuthors() -> String? {
         guard let proposal = self.proposal,
             let authors = proposal.authors,
             authors.count > 0 else {
@@ -83,12 +128,12 @@ class EvolutionTableViewCell: UITableViewCell, CellProtocol {
         let names: [String] = authors.flatMap({ $0.name })
         
         var detail = names.count > 1 ? "Authors" : "Author"
-        detail = "\(detail): \(names.joined(separator: ", "))"
+        detail = "\(detail):" + String.doubleSpace + names.joined(separator: ", ").tag(.value)
         
         return detail
     }
     
-    func renderBugs() -> String? {
+    private func renderBugs() -> String? {
         guard let proposal = self.proposal,
             let bugs = proposal.bugs,
             bugs.count > 0 else {
@@ -110,7 +155,7 @@ class EvolutionTableViewCell: UITableViewCell, CellProtocol {
         })
         
         var detail = names.count > 1 ? "Bugs" : "Bug"
-        detail = "\(detail): \(names.joined(separator: ", "))"
+        detail = "\(detail):" + String.doubleSpace + names.joined(separator: ", ").tag(.value)
         
         return detail
     }
