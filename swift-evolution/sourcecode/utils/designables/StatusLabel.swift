c5cb59201c248ec37a55ea6f8c70debc63a45a3b
Thiago Holanda <unnamedd@gmail.com>
2017-03-04 01:50:32 -0300

    changed the top and left o BorderedLabel on edgeInset

--- swift-evolution/sourcecode/utils/designables/StatusLabel.swift
+++ swift-evolution/sourcecode/utils/designables/StatusLabel.swift
@@ -3,7 +3,7 @@ import UIKit
 class StatusLabel: BorderedLabel {
 
     override func drawText(in rect: CGRect) {
-        let edgeInsets = UIEdgeInsetsInsetRect(rect, UIEdgeInsets(top: 3, left: 8, bottom: 3, right: 8))
+        let edgeInsets = UIEdgeInsetsInsetRect(rect, UIEdgeInsets(top: 0, left: 8, bottom: 3, right: 8))
         
         super.drawText(in: edgeInsets)
     }
