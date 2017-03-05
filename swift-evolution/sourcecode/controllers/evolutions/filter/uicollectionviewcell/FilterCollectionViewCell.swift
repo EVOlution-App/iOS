6f867bd8c589f5ce0af4002c75c1943510159c04
Thiago Holanda <unnamedd@gmail.com>
2017-03-05 00:27:55 -0300

    modified location from some classes and UI

---
+++ swift-evolution/sourcecode/controllers/evolutions/filter/uicollectionviewcell/FilterCollectionViewCell.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class FilterCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
