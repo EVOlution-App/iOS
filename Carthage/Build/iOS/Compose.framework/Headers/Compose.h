08079d21e3bdb0fdebf5ff5c629583c0bc1e670a
Thiago Holanda <unnamedd@gmail.com>
2017-03-05 03:07:36 -0300

    added Compose library to project

---
+++ Carthage/Build/iOS/Compose.framework/Headers/Compose.h
@@ -0,0 +1,19 @@
+//
+//  Compose.h
+//  Compose
+//
+//  Created by Bruno Bilescky on 10/10/16.
+//  Copyright © 2016 VivaReal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Compose.
+FOUNDATION_EXPORT double ComposeVersionNumber;
+
+//! Project version string for Compose.
+FOUNDATION_EXPORT const unsigned char ComposeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Compose/PublicHeader.h>
+
+
