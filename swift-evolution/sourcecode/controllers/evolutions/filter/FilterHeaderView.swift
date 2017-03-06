a4599c6d81c8f6499b022548d5c6e260416e86cf
Thiago Holanda <unnamedd@gmail.com>
2017-03-06 03:02:30 -0300

    #3 implemented FilterHeaderView

--- swift-evolution/sourcecode/controllers/evolutions/filter/FilterHeaderView.swift
+++ swift-evolution/sourcecode/controllers/evolutions/filter/FilterHeaderView.swift
@@ -2,12 +2,40 @@ import UIKit
 
 class FilterHeaderView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    @IBOutlet private weak var statusFilterView: FilterListGenericView!
+    @IBOutlet private weak var languageVersionFilterView: FilterListGenericView!
+    
+    @IBOutlet private weak var statusFilterViewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var languageVersionFilterViewHeightConstraint: NSLayoutConstraint!
+    
+    var statusSource: [StatusState] = [] {
+        didSet {
+            self.statusFilterView.type = .version
+            self.statusFilterView.dataSource = self.statusSource
+        }
     }
-    */
+    
+    var languageVersionSource: [String] = [] {
+        didSet {
+            self.languageVersionFilterView.type = .status
+            self.languageVersionFilterView.dataSource = self.languageVersionSource
+        }
+    }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
 
+    }
+    
+    override func layoutSubviews() {
+        self.languageVersionFilterViewHeightConstraint.constant = self.languageVersionFilterView.height
+        self.languageVersionFilterView.setNeedsUpdateConstraints()
+        
+        self.statusFilterViewHeightConstraint.constant = self.statusFilterView.height
+        self.statusFilterView.setNeedsUpdateConstraints()
+        
+        
+    }
+    
 }
