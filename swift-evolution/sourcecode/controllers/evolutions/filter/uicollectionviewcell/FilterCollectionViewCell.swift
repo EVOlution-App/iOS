852d16ba933e382e8248cb0c93cf582d8d62fd88
Thiago Holanda <unnamedd@gmail.com>
2017-03-06 03:01:27 -0300

    #3 implemented selected state to FilterCollectionViewCell

--- swift-evolution/sourcecode/controllers/evolutions/filter/uicollectionviewcell/FilterCollectionViewCell.swift
+++ swift-evolution/sourcecode/controllers/evolutions/filter/uicollectionviewcell/FilterCollectionViewCell.swift
@@ -2,9 +2,28 @@ import UIKit
 
 class FilterCollectionViewCell: UICollectionViewCell {
 
+    @IBOutlet private weak var statusLabel: StatusLabel!
+    
+    var text: String? = nil {
+        didSet {
+            self.statusLabel.text = text
+        }
+    }
+    
+    override var isSelected: Bool {
+        didSet {
+            self.statusLabel.selected = isSelected
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+    
+    override func prepareForReuse() {
+        self.isSelected = false
+        self.statusLabel.selected = false
     }
 
 }
