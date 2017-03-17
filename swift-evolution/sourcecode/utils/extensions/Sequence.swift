ee812287eaf578056bcbb7e19de6114c904b382e
Thiago Holanda <unnamedd@gmail.com>
2017-03-17 10:51:13 -0300

    added Sequence extension to project

---
+++ swift-evolution/sourcecode/utils/extensions/Sequence.swift
@@ -0,0 +1,87 @@
+// MARK: - Proposals Filters
+
+enum Sorting {
+    case ascending
+    case descending
+}
+
+extension Sequence where Self: RandomAccessCollection, Iterator.Element == Evolution {
+    func filter(status: StatusState) -> [Evolution] {
+        return self.filter { $0.status.state == status }
+    }
+    
+    func filter(language version: Version) -> [Evolution] {
+        return self.filter { $0.status.version == version}
+    }
+    
+    func filter(query: String) -> [Evolution] {
+        return self.filter { $0.title.contains(query) }
+    }
+    
+    func filter(by languages: [Version]) -> [Evolution] {
+        var filter: [Evolution] = []
+        
+        languages.forEach { language in
+            let list = self.filter(language: language)
+            filter.append(contentsOf: list)
+        }
+        
+        return filter
+    }
+    
+    func filter(by statuses: [StatusState]) -> [Evolution] {
+        var filter: [Evolution] = []
+        
+        statuses.forEach { state in
+            let list = self.filter(status: state)
+            filter.append(contentsOf: list.sort(.descending))
+        }
+        
+        return filter
+    }
+    
+    func index(of proposal: Evolution) -> Int? {
+        return self.index(where: { $0 == proposal }) as? Int
+    }
+
+    func sort(_ direction: Sorting) -> [Evolution] {
+        return self.sorted(by: direction == .ascending ? { $0 > $1 } : { $0 < $1 })
+    }
+    
+    mutating func removeDuplicates() -> [Evolution] {
+        var result: [Evolution] = []
+    
+        for value in self {
+            if result.index(of: value) == nil {
+                result.append(value)
+            }
+        }
+        
+        return result
+    }
+}
+
+// MARK: - Extension Sequence Status Filter
+
+extension Sequence where Self: RangeReplaceableCollection, Self: RandomAccessCollection, Iterator.Element == StatusState {
+    mutating func remove(_ status: StatusState) -> Bool {
+        if let index = self.index(where: { $0 == status }) {
+            self.remove(at: index)
+            return true
+        }
+        
+        return false
+    }
+}
+
+// MARK: - Extension Sequence String
+extension Sequence where Self: RangeReplaceableCollection, Self: RandomAccessCollection, Iterator.Element == String {
+    mutating func remove(string: String) -> Bool {
+        if let index = self.index(where: { $0 == string }) {
+            self.remove(at: index)
+            return true
+        }
+        
+        return false
+    }
+}
