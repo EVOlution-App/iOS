68a9d59f74343d15ffbba21cce384054d9645c7c
Thiago Holanda <unnamedd@gmail.com>
2017-03-19 05:13:14 -0300

    added github username property into Person model

--- swift-evolution/sourcecode/models/Person.swift
+++ swift-evolution/sourcecode/models/Person.swift
@@ -4,11 +4,13 @@ import Unbox
 struct Person {
     let name: String?
     let link: String?
+    let username: String?
 }
 
 extension Person: Unboxable {
     init(unboxer: Unboxer) throws {
         self.name = unboxer.unbox(key: "name")
         self.link = unboxer.unbox(key: "link")
+        self.username = unboxer.unbox(key: "link", formatter: GithubUserFormatter())
     }
 }
