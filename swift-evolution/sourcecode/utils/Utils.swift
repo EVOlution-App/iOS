d9f5d74a2ac24b4099e0b60199faab0c0c39b52a
Thiago Holanda <unnamedd@gmail.com>
2017-03-19 05:14:41 -0300

    added bugID regex

--- swift-evolution/sourcecode/utils/Utils.swift
+++ swift-evolution/sourcecode/utils/Utils.swift
@@ -48,6 +48,10 @@ struct Config {
             static var proposalID: String {
                 return "SE-([0-9]+)"
             }
+            
+            static var bugID: String {
+                return "SR-([0-9]+)"
+            }
         }
     }
 }
