642b6327894e02936d4156d8404033784d095f85
Bruno Guidolim <ac@ac-bguidolim-mac.local>
2017-03-29 16:46:14 -0300

    Issue #25 - Added network monitor

--- swift-evolution/sourcecode/AppDelegate.swift
+++ swift-evolution/sourcecode/AppDelegate.swift
@@ -9,6 +9,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+        LoadingMonitor.register()
+        
         let font = UIFont(name: "HelveticaNeue-Thin", size: 25)!
         UINavigationBar.appearance().titleTextAttributes = [NSFontAttributeName: font, NSForegroundColorAttributeName: UIColor.Proposal.darkGray]
         
