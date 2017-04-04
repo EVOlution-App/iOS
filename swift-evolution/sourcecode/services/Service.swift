db0cc1b62e4de7d969015b30c9dc9e41399a4ba7
Thiago Holanda <unnamedd@gmail.com>
2017-04-04 01:52:30 -0300

    added request to simple keyValue response to Service.swift

--- swift-evolution/sourcecode/services/Service.swift
+++ swift-evolution/sourcecode/services/Service.swift
@@ -3,6 +3,7 @@ import UIKit
 class Service {
     typealias CompletionObject = (_ error: Error?, _ object: Data?) -> Swift.Void
     typealias CompletionString = (_ error: Error?, _ string: String?) -> Swift.Void
+    typealias CompletionKeyValue = (_ error: Error?, _ keyValue: [String: Any]?) -> Swift.Void
     typealias CompletionListKeyValue = (_ error: Error?, _ listKeyValue: [[String: Any]]?) -> Swift.Void
     
     static func requestList(_ url: String, completion: @escaping CompletionListKeyValue) {
@@ -22,6 +23,30 @@ class Service {
             
             do {
                 if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [[String: Any]] {
+                    completion(nil, json)
+                }
+            }
+            catch let error as NSError {
+                completion(error, nil)
+            }
+        }
+    }
+    
+    static func requestKeyValue(_ url: String, completion: @escaping CompletionKeyValue) {
+        guard url != "" else {
+            return
+        }
+        
+        self.request(url: url) { error, data in
+            guard error == nil, let data = data else {
+                print("error=\(String(describing: error))")
+                completion(error, nil)
+                
+                return
+            }
+            
+            do {
+                if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
                     completion(nil, json)
                 }
             }
