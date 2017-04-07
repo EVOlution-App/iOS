25cfabbbc7458ec11396c0dee186e7821b314b3d
Thiago Holanda <unnamedd@gmail.com>
2017-04-07 04:59:19 -0300

    added extension to navigationcontroller

---
+++ swift-evolution/sourcecode/utils/extensions/UINavigationController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+extension UINavigationController {
+    public func backgroundTransparent() {
+        self.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        self.navigationBar.shadowImage = UIImage()
+        self.navigationBar.isTranslucent = true
+        self.view.backgroundColor = UIColor.clear
+    }
+}
