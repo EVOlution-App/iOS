d97f686a3080daf01a2f992d7278c9a37907a114
Thiago Holanda <unnamedd@gmail.com>
2017-04-08 18:32:17 -0300

    implemented search by id on dictionary

---
+++ swift-evolution/sourcecode/utils/extensions/Dictionary.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+extension Dictionary where Key: ExpressibleByStringLiteral, Value: Searchable {
+    /**
+     Returns an object found on `[String: Person]` structure
+     If any object could found, this method will return nil
+     
+    - parameter username: login from user
+    - returns: person object found
+     */
+    func get(username: String) -> Person? {
+        var item: Person? = nil
+        
+        self.forEach { key, value in
+            if value is Person, let person = value as? Person,
+                person.username == username {
+                item = person
+            }
+        }
+        
+        return item
+    }
+}
