a96bac7a743b89273223ee83e679de85fd6a50b3
Thiago Holanda <unnamedd@gmail.com>
2017-06-20 12:54:59 -0300

    created public struct to release version

--- swift-evolution/sourcecode/utils/Environment.swift
+++ swift-evolution/sourcecode/utils/Environment.swift
@@ -2,6 +2,7 @@ import Foundation
 
 public struct Environment {
     
+    
     private static var settings: [String: Any]? {
         guard
             let dict = Bundle.main.infoDictionary,
@@ -20,5 +21,37 @@ public struct Environment {
         }
         
         return title
+    }
+
+    public struct Release {
+        public static var name: String? {
+            guard
+                let dict = Bundle.main.infoDictionary,
+                let name = dict["CFBundleDisplayName"] as? String else {
+                    return nil
+            }
+            
+            return name
+        }
+        
+        public static var version: String? {
+            guard
+                let dict = Bundle.main.infoDictionary,
+                let name = dict["CFBundleShortVersionString"] as? String else {
+                    return nil
+            }
+            
+            return name
+        }
+        
+        public static var build: String? {
+            guard
+                let dict = Bundle.main.infoDictionary,
+                let name = dict["CFBundleVersion"] as? String else {
+                    return nil
+            }
+            
+            return name
+        }
     }
 }
