99b56788ad73dab85d98d07dfd95f4c40b26d460
Thiago Holanda <unnamedd@gmail.com>
2017-09-28 00:50:13 +0200

    adjusts to conforme to Swift 4 and SwiftLint

--- swift-evolution/sourcecode/utils/extensions/UITableView.swift
+++ swift-evolution/sourcecode/utils/extensions/UITableView.swift
@@ -2,14 +2,14 @@ import UIKit
 
 extension UITableView {
     
-    func registerNib<T: UITableViewCell>(withClass cellClass: T.Type) where T: ReusableCellIdentifiable {
+    func registerNib<T: UITableViewCell>(withClass cellClass: T.Type) {
         register(
             Config.Nib.loadNib(name: T.cellIdentifier),
             forCellReuseIdentifier: T.cellIdentifier
         )
     }
     
-    func registerClass<T: UITableViewCell>(_ cellClass: T.Type) where T: ReusableCellIdentifiable {
+    func registerClass<T: UITableViewCell>(_ cellClass: T.Type) {
         register(cellClass.self, forCellReuseIdentifier: cellClass.cellIdentifier)
     }
     
