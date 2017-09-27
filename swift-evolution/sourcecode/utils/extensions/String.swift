8c7e3901eb4611d724671d6bfef6ed936df83893
Thiago Holanda <unnamedd@gmail.com>
2017-09-28 00:46:38 +0200

    removed unnecessary method to get initials from two words

--- swift-evolution/sourcecode/utils/extensions/String.swift
+++ swift-evolution/sourcecode/utils/extensions/String.swift
@@ -128,38 +128,4 @@ extension String {
         
         return name
     }
-    
-    /**
-     Return only initials from first and last word from string.
-     
-     **Best for:** _initials from name_
-     ```
-     let name = "Swift Evolution"
-     print(name.initials) // Optional("SE")
-     ```
-     */
-    var initials: String? {
-        guard self.firstLast.characters.count > 0 else {
-            return nil
-        }
-        
-        var initials: String?
-        let list = self.components(separatedBy: .whitespaces)
-
-        if let first = list.first, let last = list.last, list.count > 1 {
-            let findex = first.index(after: first.startIndex)
-            let lindex = last.index(after: last.startIndex)
-            
-            let first = first.substring(to: findex)
-            let last = last.substring(to: lindex)
-
-            initials = first + last
-        }
-        else if let item = list.first, list.count == 1 {
-            let index = item.index(after: item.startIndex)
-            initials = item.substring(to: index)
-        }
-        
-        return initials
-    }
 }
