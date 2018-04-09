ea68990143a946158a195cca83135fe50e4e8f54
Thiago Holanda <unnamedd@gmail.com>
2018-04-09 02:49:52 +0200

    implemented Service+CloudKit
    it will be responsible for request CKRecordID from CloudKit, from it, we will use the recordName. This will allows to have an invisible authentication

---
+++ swift-evolution/sourcecode/services/Service+CloudKit.swift
@@ -0,0 +1,29 @@
+import CloudKit
+import KeychainAccess
+
+struct CloudKitService {
+    typealias UserClosure = (ServiceResult<User>) -> Swift.Void
+
+    static func user(completion: @escaping UserClosure) {
+        let container = CKContainer.default()
+        container.fetchUserRecordID { result, error in
+            if let error = error {
+                completion(.failure(error))
+            }
+            
+            guard let recordID = result else {
+                completion(.failure(ServiceError.unknownState))
+                return
+            }
+            
+            let bundleID = Environment.bundleID ?? "io.swift-evolution.app"
+            let keychain = Keychain(service: bundleID).synchronizable(true)
+            
+            let userID = recordID.recordName
+            keychain["currentUser"] = userID
+            
+            let user = User(id: userID)
+            completion(.success(user))
+        }
+    }
+}
