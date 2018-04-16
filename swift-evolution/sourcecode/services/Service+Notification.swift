69c15c9aa413cdc6efbd41bd5cfd29267295115f
Thiago Holanda <unnamedd@gmail.com>
2018-04-17 00:11:58 +0200

    renamed Notifications model

--- swift-evolution/sourcecode/services/Service+Notification.swift
+++ swift-evolution/sourcecode/services/Service+Notification.swift
@@ -1,22 +1,22 @@
 import Foundation
 
-struct NotificationService {
-    typealias AddSuccessClosure = (ServiceResult<Device>) -> Swift.Void
+struct NotificationsService {
+    typealias AddSuccessClosure = (ServiceResult<Notifications.Device>) -> Swift.Void
     
     @discardableResult
-    static func add(_ device: Device, completion: @escaping AddSuccessClosure) -> URLSessionDataTask? {
+    static func add(_ device: Notifications.Device, completion: @escaping AddSuccessClosure) -> URLSessionDataTask? {
         guard let params = device.asDictionary() else {
             return nil
         }
         
         let key = Environment.Keys.notification ?? ""
-        let url = Config.Base.URL.Notification.add
+        let url = Config.Base.URL.Notifications.add
         
         let headers: [Header: String] = [.authorization: key, .contentType: MimeType.applicationJSON.rawValue]
         let request = RequestSettings(url, method: .post, params: params, headers: headers)
         
         let task = Service.dispatch(request) { result in
-            let value = result.flatMap { try JSONDecoder().decode(Device.self, from: $0) }
+            let value = result.flatMap { try JSONDecoder().decode(Notifications.Device.self, from: $0) }
             completion(value)
         }
         
