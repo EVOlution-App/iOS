ea4723ea0b34612c5deef2425f10c369ecdf24c4
Thiago Holanda <unnamedd@gmail.com>
2018-04-17 02:48:05 +0200

    implemented request to update user’s tags

--- swift-evolution/sourcecode/services/Service+Notification.swift
+++ swift-evolution/sourcecode/services/Service+Notification.swift
@@ -2,6 +2,11 @@ import Foundation
 
 struct NotificationsService {
     typealias AddSuccessClosure = (ServiceResult<Notifications.Device>) -> Swift.Void
+    typealias UpdateTagsClosure = (ServiceResult<User>) -> Swift.Void
+    static var authorizationHeader: [Header: String] {
+        let key = Environment.Keys.notification ?? ""
+        return [.authorization: key, .contentType: MimeType.applicationJSON.rawValue]
+    }
     
     @discardableResult
     static func add(_ device: Notifications.Device, completion: @escaping AddSuccessClosure) -> URLSessionDataTask? {
@@ -9,14 +14,34 @@ struct NotificationsService {
             return nil
         }
         
-        let key = Environment.Keys.notification ?? ""
         let url = Config.Base.URL.Notifications.add
-        
-        let headers: [Header: String] = [.authorization: key, .contentType: MimeType.applicationJSON.rawValue]
-        let request = RequestSettings(url, method: .post, params: params, headers: headers)
+        let request = RequestSettings(url,
+                                      method: .post,
+                                      params: params,
+                                      headers: NotificationsService.authorizationHeader)
         
         let task = Service.dispatch(request) { result in
             let value = result.flatMap { try JSONDecoder().decode(Notifications.Device.self, from: $0) }
+            completion(value)
+        }
+        
+        return task
+    }
+    
+    @discardableResult
+    static func updateTags(to user: User, completion: @escaping UpdateTagsClosure) -> URLSessionDataTask? {
+        guard let params = user.asDictionary() else {
+            return nil
+        }
+        
+        let url = Config.Base.URL.Notifications.user(id: user.id)
+        let request = RequestSettings(url,
+                                      method: .put,
+                                      params: params,
+                                      headers: NotificationsService.authorizationHeader)
+        
+        let task = Service.dispatch(request) { result in
+            let value = result.flatMap { try JSONDecoder().decode(User.self, from: $0) }
             completion(value)
         }
         
