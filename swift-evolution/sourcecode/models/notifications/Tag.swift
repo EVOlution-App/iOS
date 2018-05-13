287637dd0389144aaf9f53ffe9e2fe1bae5c321e
Thiago Holanda <unnamedd@gmail.com>
2018-05-14 00:54:46 +0200

    added subscribed property to Tag model

--- swift-evolution/sourcecode/models/notifications/Tag.swift
+++ swift-evolution/sourcecode/models/notifications/Tag.swift
@@ -5,12 +5,13 @@ extension Notifications {
         let id: String
         let name: String
         let identifier: String
-        let subscribed: Bool
+        let subscribed: Bool?
         
-        enum Keys: String, CodingKey {
+        enum CodingKeys: String, CodingKey {
             case id = "_id"
             case name
             case identifier
+            case subscribed
         }
     }
 }
