9b0219f5efe7179715fc56e1462cd39e755d6345
Thiago Holanda <unnamedd@gmail.com>
2018-05-17 03:04:00 +0200

    add CodingKeys to Responde model

--- swift-evolution/sourcecode/models/Response.swift
+++ swift-evolution/sourcecode/models/Response.swift
@@ -3,4 +3,9 @@ import Foundation
 struct Response: Codable {
     let statusCode: Int
     let reason: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case statusCode
+        case reason
+    }
 }
