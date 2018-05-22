79b175c3ff49deda716b96f70367445a348ce143
Thiago Holanda <unnamedd@gmail.com>
2018-05-22 02:39:31 +0200

    add navigation class

---
+++ swift-evolution/sourcecode/utils/Navigation.swift
@@ -0,0 +1,62 @@
+import Foundation
+
+public enum Host {
+    case proposal
+    case profile
+    case implementation
+    
+    init?(_ value: String?) {
+        guard let value = value else {
+            return nil
+        }
+        
+        switch value {
+        case "proposal":
+            self = .proposal
+            
+        case "profile":
+            self = .profile
+            
+        case "implementation":
+            self = .implementation
+            
+        default:
+            return nil
+        }
+    }
+}
+
+final class Navigation {
+    static let shared = Navigation()
+    
+    var host: Host?
+    var value: String?
+
+    var isClear: Bool {
+        return host == nil
+    }
+    
+    func clear() {
+        host = nil
+        value = nil
+    }
+}
+
+extension Navigation: CustomStringConvertible {
+    var description: String {
+        var value = ""
+        
+        if let host = host {
+            value += "Host: \(host)"
+        }
+        
+        if let content = self.value {
+            value += value.count > 0 ? ", " : ""
+            value += "Value: \(content)"
+        }
+        
+        value = "Navigation<\(value)>"
+        
+        return value
+    }
+}
